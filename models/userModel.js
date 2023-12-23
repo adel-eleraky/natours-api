196ee370098c610413842fd1b6fba1316c766418
el3raky <kameladel339@gmail.com>
2023-12-23 14:56:51 +0200

    added role attr to user Model

--- models/userModel.js
+++ models/userModel.js
@@ -20,6 +20,11 @@ const userSchema = new mongoose.Schema({
             message: "enter valid email address"
         }
     },
+    role: {
+        type: String,
+        enum: ["user" , "guide" , "lead-guide" , "admin"],
+        default: "user"
+    },
     password: {
         type: String,
         required: [true, 'Please enter your password'],
