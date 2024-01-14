f07820c441c3ec66a4b85180aff6e2368bdc8816
el3raky <kameladel339@gmail.com>
2024-01-14 10:03:06 +0200

    added update user's data controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,7 @@
+const { validationResult } = require("express-validator");
 const User = require("../models/userModel");
 const asyncHandler = require("../utils/asyncHandler");
+const displayValidationErrors = require("../utils/validationErrors");
 
 exports.getAllUsers = asyncHandler( async (req, res, next) => {
 
@@ -17,20 +19,6 @@ exports.getAllUsers = asyncHandler( async (req, res, next) => {
 exports.getUser = (req, res) => {
     res.json({ status: "success", data: "get single user" })
 }
-
-exports.updateUser = asyncHandler( async (req, res , next) => {
-
-    const updatedUser = await User.findByIdAndUpdate( req.params.id , req.body , {
-        new: true,
-        runValidators: true
-    })
-
-    res.status(200).json({
-        status: "success",
-        message: "user updated successfully"
-    })
-})
-
 
 exports.createUser = asyncHandler( async (req, res) => {
 
@@ -56,3 +44,27 @@ exports.deleteUser = asyncHandler( async (req, res ,next) => {
         data: null
     })
 });
+
+
+exports.updateUser = asyncHandler( async (req , res , next ) => {
+
+    // 1)  check if there is errors from express-validator
+    const errors = validationResult(req)
+    if(! errors.isEmpty()){
+        return displayValidationErrors(errors , res)
+    }
+
+    //2) update user data
+    const updateData = { email: req.body.email , name: req.body.name }
+    const updatedUser = await User.findByIdAndUpdate( req.user.id , updateData , {
+        new: true,
+        runValidators: true
+    })
+
+    // send response to the client
+    res.status(200).json({
+        status: "success",
+        message: "user updated successfully",
+        updatedUser
+    })
+})
