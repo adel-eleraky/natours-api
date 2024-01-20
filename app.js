941db4728f9774f38c1dda35ab90cc3ad70196ca
el3raky <kameladel339@gmail.com>
2024-01-20 15:04:51 +0200

    imported global middleware module

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require("express")
-const morgan = require("morgan")
-const cookieParser = require("cookie-parser")
+const setupGlobalMiddleware = require("./middlewares/globalMiddlewares")
 const AppError = require('./utils/appError')
 const globalErrorMiddleware = require("./middlewares/errorMiddleware")
 const tourRouter = require("./routes/tourRouter")
@@ -8,15 +7,8 @@ const userRouter = require("./routes/userRouter")
 
 const app = express()
 
-// middlewares
-if (process.env.NODE_ENV === "development") {
-    app.use(morgan("dev"))
-}
-
-app.use(express.json())
-app.use(express.static(`${__dirname}/public`))
-app.use(cookieParser())
-
+// global middlewares
+setupGlobalMiddleware(app)
 
 // Routes
 app.use("/api/v1/tours", tourRouter)
