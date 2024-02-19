f7435a9d86f9ff399f17ee0f3033b0959fbbddae
el3raky <kameladel339@gmail.com>
2024-02-20 01:04:46 +0200

    formate the code

--- models/userModel.js
+++ models/userModel.js
@@ -5,16 +5,16 @@ const crypto = require("crypto")
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: [true , 'Please enter your name'],
-        minLength: [ 2 , "name must be more than 2 characters"],
-        maxLength: [ 32 , "name must be less than 32 characters"],
+        required: [true, 'Please enter your name'],
+        minLength: [2, "name must be more than 2 characters"],
+        maxLength: [32, "name must be less than 32 characters"],
     },
     email: {
         type: String,
         required: [true, 'Please enter your email'],
         unique: true,
         validate: {
-            validator: function(val){
+            validator: function (val) {
                 let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
                 return emailRegex.test(val)
             },
@@ -23,20 +23,21 @@ const userSchema = new mongoose.Schema({
     },
     role: {
         type: String,
-        enum: ["user" , "guide" , "lead-guide" , "admin"],
+        enum: ["user", "guide", "lead-guide", "admin"],
         default: "user"
     },
     password: {
         type: String,
         required: [true, 'Please enter your password'],
-        minLength: [ 8 , "password must be more than 8 character"],
+        minLength: [8, "password must be more than 8 character"],
+        select: false
     },
     passwordConfirm: {  // only for validation , only work on create() , save()
         type: String,
         required: true,
-        minLength: [ 8 , "password must be more than 8 character"],
+        minLength: [8, "password must be more than 8 character"],
         validate: {
-            validator: function(val) {
+            validator: function (val) {
                 return this.password === val
             },
             message: "password doesn't match"
@@ -52,32 +53,33 @@ const userSchema = new mongoose.Schema({
 })
 
 // hashing password before save in database
-userSchema.pre('save' , async function(next) {
-    if(! this.isModified("password")) return next(); // run this if only password isn't modified
+userSchema.pre('save', async function (next) {
+    if (!this.isModified("password")) return next(); // run this if only password isn't modified
 
-    this.password = await bcrypt.hash(this.password , 10)  // hash password
+    this.password = await bcrypt.hash(this.password, 10)  // hash password
     this.passwordConfirm = undefined; // delete passwordConfirm field
     next();
 })
 
 // update passwordChangedAt property when update password
-userSchema.pre("save" , function(next) {
-    if(! this.isModified("password") || this.isNew) return next();
+userSchema.pre("save", function (next) {
+    if (!this.isModified("password") || this.isNew) return next();
 
     this.passwordChangedAt = Date.now()
     next()
 })
 
-// check password is correct when login
-userSchema.methods.checkPassword = async function(inputPassword , userPassword) {
 
-    return await bcrypt.compare(inputPassword , userPassword)
+// check password is correct when login
+userSchema.methods.checkPassword = async function (inputPassword, userPassword) {
+
+    return await bcrypt.compare(inputPassword, userPassword)
 }
 
 // check if the password changed after token was issued
-userSchema.methods.changedPasswordAfter = function(JWTTimestamp) {
+userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
 
-    if(this.passwordChangedAt) {
+    if (this.passwordChangedAt) {
         const passwordTimestamp = parseInt(this.passwordChangedAt.getTime() / 1000)
         return passwordTimestamp > JWTTimestamp
     }
@@ -85,7 +87,7 @@ userSchema.methods.changedPasswordAfter = function(JWTTimestamp) {
 }
 
 // generate && save password reset token in database
-userSchema.methods.createPwdToken = function() {
+userSchema.methods.createPwdToken = function () {
     const resetToken = crypto.randomBytes(32).toString("hex")
 
     this.passwordResetToken = crypto.createHash("sha256").update(resetToken).digest("hex") // hash password Token 
@@ -94,5 +96,5 @@ userSchema.methods.createPwdToken = function() {
     return resetToken;
 }
 
-const userModel = mongoose.model('User' , userSchema)
+const userModel = mongoose.model('User', userSchema)
 module.exports = userModel
