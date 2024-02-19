1b99250d6d3541697e5e360b3552a19997efda51
el3raky <kameladel339@gmail.com>
2024-02-20 00:59:02 +0200

    used sendResponse func to response handling

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,41 +2,48 @@ const { validationResult } = require("express-validator");
 const User = require("../models/userModel");
 const asyncHandler = require("../utils/asyncHandler");
 const displayValidationErrors = require("../utils/validationErrors");
+const sendResponse = require("../utils/sendResponse");
 
-exports.getAllUsers = asyncHandler( async (req, res, next) => {
+exports.getAllUsers = asyncHandler(async (req, res, next) => {
 
     const users = await User.find().select("-password")
 
-    res.status(200).json({
-        status: 'success',
+
+    // 3) send response to the client
+    sendResponse(res, 200, {
         result: users.length,
-        data: {
-            users
-        } 
-    });
-});
-
-exports.getUser = (req, res) => {
-    res.json({ status: "success", data: "get single user" })
-}
-
-exports.createUser = asyncHandler( async (req, res) => {
-
-    const userData = { ...req.body }
-    
-    const newUser = await User.create(userData)
-    
-    res.status(201).json({
-        status: "success",
-        data: userData
+        data: { users }
     })
 });
 
-exports.deleteUser = asyncHandler( async (req, res ,next) => {
+exports.getUser = (req, res) => {
+    // 3) send response to the client
+    sendResponse(res, 200, {
+        data: "get single user"
+    })
+}
+
+exports.createUser = asyncHandler(async (req, res) => {
+
+    const userData = { ...req.body }
+
+    const newUser = await User.create(userData)
+
+    // 3) send response to the client
+    sendResponse(res, 201, {
+        status: "success",
+        data: {
+            newUser
+        }
+    })
+});
+
+exports.deleteUser = asyncHandler(async (req, res, next) => {
 
     await User.findByIdAndDelete(req.user.id)
 
-    res.status(204).json({
+    // 3) send response to the client
+    sendResponse(res, 204, {
         status: "success",
         message: "user deleted successfully",
         data: null
@@ -44,23 +51,24 @@ exports.deleteUser = asyncHandler( async (req, res ,next) => {
 });
 
 
-exports.updateUser = asyncHandler( async (req , res , next ) => {
+exports.updateUser = asyncHandler(async (req, res, next) => {
 
     // 1)  check if there is errors from express-validator
     const errors = validationResult(req)
-    if(! errors.isEmpty()){
-        return displayValidationErrors(errors , res)
+    if (!errors.isEmpty()) {
+        return displayValidationErrors(errors, res)
     }
 
     //2) update user data
-    const updateData = { email: req.body.email , name: req.body.name }
-    const updatedUser = await User.findByIdAndUpdate( req.user.id , updateData , {
+    const { email, name, role , test } = req.body
+    const updateData = { email, name, role ,test}
+    const updatedUser = await User.findByIdAndUpdate(req.user.id, updateData, {
         new: true,
         runValidators: true
     })
 
     // send response to the client
-    res.status(200).json({
+    sendResponse(res, 200, {
         status: "success",
         message: "user updated successfully",
         updatedUser
