58bf9f9f7fae4f1caf253e49894592f617c4b6b4
el3raky <kameladel339@gmail.com>
2024-02-20 23:46:22 +0200

    use create & delete & update factory handler for reviews

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,6 +1,7 @@
 const Review = require("./../models/reviewModel")
 const sendResponse = require("./../utils/sendResponse")
 const asyncHandler = require("./../utils/asyncHandler")
+const factory = require("./handlerFactory");
 
 // get all reviews handler
 exports.getAllReviews = asyncHandler(async (req, res, next) => {
@@ -17,20 +18,31 @@ exports.getAllReviews = asyncHandler(async (req, res, next) => {
 })
 
 // create review handler
-exports.createReview = asyncHandler(async (req, res, next) => {
+// exports.createReview = asyncHandler(async (req, res, next) => {
 
-    let { review, rating, tour, user } = req.body
-    if (!tour) tour = req.params.tourId
-    if (!user) user = req.user.id
+//     let { review, rating, tour, user } = req.body
+//     if (!tour) tour = req.params.tourId
+//     if (!user) user = req.user.id
 
-    const updateData = { review, rating, tour, user }
-    const newReview = await Review.create(updateData)
+//     const updateData = { review, rating, tour, user }
+//     const newReview = await Review.create(updateData)
 
-    sendResponse(res, 201, {
-        status: "success",
-        message: "review created successfully",
-        data: {
-            review: newReview
-        }
-    })
-})
+//     sendResponse(res, 201, {
+//         status: "success",
+//         message: "review created successfully",
+//         data: {
+//             review: newReview
+//         }
+//     })
+// })
+
+exports.setTourUserIds = (req, res, next) => {
+
+    if (!req.body.tour) req.body.tour = req.params.tourId
+    if (!req.body.user) req.body.user = req.user.id
+    next()
+}
+
+exports.createReview = factory.createOne(Review)
+exports.deleteReview = factory.deleteOne(Review)
+exports.updateReview = factory.updateOne(Review)
