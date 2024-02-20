b4cbdd273d3935b5a33236f33df24c6f1ced8613
el3raky <kameladel339@gmail.com>
2024-02-20 23:32:31 +0200

    added delete & update factory for user

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,6 +3,7 @@ const User = require("../models/userModel");
 const asyncHandler = require("../utils/asyncHandler");
 const displayValidationErrors = require("../utils/validationErrors");
 const sendResponse = require("../utils/sendResponse");
+const factory = require("./handlerFactory");
 
 exports.getAllUsers = asyncHandler(async (req, res, next) => {
 
@@ -38,39 +39,43 @@ exports.createUser = asyncHandler(async (req, res) => {
     })
 });
 
-exports.deleteUser = asyncHandler(async (req, res, next) => {
 
-    await User.findByIdAndDelete(req.user.id)
+// delete user
+exports.deleteUser = factory.deleteOne(User)
+// exports.deleteUser = asyncHandler(async (req, res, next) => {
 
-    // 3) send response to the client
-    sendResponse(res, 204, {
-        status: "success",
-        message: "user deleted successfully",
-        data: null
-    })
-});
+//     await User.findByIdAndDelete(req.user.id)
+
+//     // 3) send response to the client
+//     sendResponse(res, 204, {
+//         status: "success",
+//         message: "user deleted successfully",
+//         data: null
+//     })
+// });
 
 
-exports.updateUser = asyncHandler(async (req, res, next) => {
+exports.updateUser = factory.updateOne(User)
+// exports.updateUser = asyncHandler(async (req, res, next) => {
 
-    // 1)  check if there is errors from express-validator
-    const errors = validationResult(req)
-    if (!errors.isEmpty()) {
-        return displayValidationErrors(errors, res)
-    }
+    // // 1)  check if there is errors from express-validator
+    // const errors = validationResult(req)
+    // if (!errors.isEmpty()) {
+    //     return displayValidationErrors(errors, res)
+    // }
 
-    //2) update user data
-    const { email, name, role , test } = req.body
-    const updateData = { email, name, role ,test}
-    const updatedUser = await User.findByIdAndUpdate(req.user.id, updateData, {
-        new: true,
-        runValidators: true
-    })
+//     //2) update user data
+//     const { email, name, role } = req.body
+//     const updateData = { email, name, role }
+//     const updatedUser = await User.findByIdAndUpdate(req.user.id, updateData, {
+//         new: true,
+//         runValidators: true
+//     })
 
-    // send response to the client
-    sendResponse(res, 200, {
-        status: "success",
-        message: "user updated successfully",
-        updatedUser
-    })
-})
+//     // send response to the client
+//     sendResponse(res, 200, {
+//         status: "success",
+//         message: "user updated successfully",
+//         updatedUser
+//     })
+// })
