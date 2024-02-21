5b820a342cca76f5268963f99d61e72df5610104
el3raky <kameladel339@gmail.com>
2024-02-22 00:45:02 +0200

    created function to update ratingAverage , numAverage in tours , when create new review

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,35 +1,36 @@
 const mongoose = require("mongoose");
+const Tour = require("./tourModel")
 
-reviewSchema = new mongoose.Schema({
-    review: {
-        type: String,
-        required: [true, "review can not be empty"]
+    reviewSchema = new mongoose.Schema({
+        review: {
+            type: String,
+            required: [true, "review can not be empty"]
+        },
+        rating: {
+            type: Number,
+            min: [1, 'rating must be above 1.0'],
+            max: [5, 'rating must be below 5.0']
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        },
+        tour: {
+            type: mongoose.Schema.ObjectId,
+            ref: "Tour",
+            required: [true, "review must belong to a tour"]
+        },
+        user: {
+            type: mongoose.Schema.ObjectId,
+            ref: "User",
+            required: [true, "review must belong to a user"]
+        },
     },
-    rating: {
-        type: Number,
-        min: [1, 'rating must be above 1.0'],
-        max: [5, 'rating must be below 5.0']
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
-    },
-    tour: {
-        type: mongoose.Schema.ObjectId,
-        ref: "Tour",
-        required: [true, "review must belong to a tour"]
-    },
-    user: {
-        type: mongoose.Schema.ObjectId,
-        ref: "User",
-        required: [true, "review must belong to a user"]
-    },
-},
-    {
-        toJSON: { virtuals: true },
-        toObject: { virtuals: true }
-    }
-)
+        {
+            toJSON: { virtuals: true },
+            toObject: { virtuals: true }
+        }
+    )
 
 reviewSchema.pre(/^find/, function (next) {
 
@@ -38,6 +39,32 @@ reviewSchema.pre(/^find/, function (next) {
         select: "name photo"
     })
     next()
+})
+
+// update tour [ ratingsQuantity , ratingsAverage ] when creating review on that tour
+reviewSchema.statics.calcAverageRatings = async function (tourId) {
+
+    const stats = await this.aggregate([ // this keyword refer to the model
+        {
+            $match: { tour: tourId }
+        },
+        {
+            $group: {
+                _id: "$tour",
+                nRatings: { $sum: 1 },
+                avgRating: { $avg: "$rating" }
+            }
+        }
+    ])
+
+    await Tour.findByIdAndUpdate(tourId, {
+        ratingsQuantity: stats[0].nRatings,
+        ratingsAverage: stats[0].avgRating
+    })
+}
+
+reviewSchema.post("save", function () {
+    this.constructor.calcAverageRatings(this.tour)  // this keyword refer to the document
 })
 
 const ReviewModel = mongoose.model("Review", reviewSchema)
