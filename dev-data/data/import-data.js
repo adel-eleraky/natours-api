e9dfb0acd438c590f6e27d611406fd69acb581a5
el3raky <kameladel339@gmail.com>
2024-02-22 00:43:56 +0200

    added users , reviews files for importing

--- dev-data/data/import-data.js
+++ dev-data/data/import-data.js
@@ -3,6 +3,8 @@ const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 
 const Tour = require("./../../models/tourModel")
+const User = require("./../../models/userModel")
+const Review = require("./../../models/reviewModel")
 const mongoose = require('mongoose');
 
 // connect to mongo atlas
@@ -16,11 +18,15 @@ mongoose.connect(DB).then((conn) => {
 });
 
 const tours = JSON.parse( fs.readFileSync(`${__dirname}/tours.json` , "utf-8" ));
+const users = JSON.parse( fs.readFileSync(`${__dirname}/users.json` , "utf-8" ));
+const reviews = JSON.parse( fs.readFileSync(`${__dirname}/reviews.json` , "utf-8" ));
 
 const importData = async() => {
     try{
 
         await Tour.create(tours)
+        await User.create(users , { validateBeforeSave: false })
+        await Review.create(reviews)
 
         console.log("data imported successfully")
     }catch(err) {
@@ -32,6 +38,8 @@ const deleteData = async() => {
     try {
 
         await Tour.deleteMany();
+        await User.deleteMany();
+        await Review.deleteMany();
 
         console.log("data deleted successfully")
     }catch(err) {
