8c399e1b44bdc14093cc628a2daef16d4b4a4253
el3raky <kameladel339@gmail.com>
2024-02-22 00:46:49 +0200

    maintain authorization for tours routes

--- routes/tourRouter.js
+++ routes/tourRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const tourController = require('./../controllers/tourController');
-const { protectRoute , restrictTo } = require("./../middlewares/authMiddleware")
+const { protectRoute, restrictTo } = require("./../middlewares/authMiddleware")
 const reviewRouter = require("./reviewRouter")
 
 const router = express.Router();
@@ -8,7 +8,7 @@ const router = express.Router();
 // param middleware
 // router.param('id', tourController.checkId); 
 
-router.use("/:tourId/reviews" , reviewRouter)
+router.use("/:tourId/reviews", reviewRouter)
 
 // get top five cheapest tours
 router
@@ -18,7 +18,7 @@ router
 // get monthly plan per year 
 router
     .route("/monthly-plan/:year")
-    .get(tourController.getMonthlyPlan)
+    .get(protectRoute, restrictTo("admin", "lead-guide", "guide"), tourController.getMonthlyPlan)
 
 // get tour stats
 router
@@ -28,14 +28,14 @@ router
 // CRUD operations on tours
 router
     .route('/')
-    .get( protectRoute , tourController.getAllTours)
-    .post(tourController.createTour);
+    .get(tourController.getAllTours)
+    .post(protectRoute, restrictTo("admin", "lead-guide"), tourController.createTour);
 
 router
     .route('/:id')
     .get(tourController.getTour)
-    .patch(tourController.updateTour)
-    .delete( protectRoute , restrictTo("admin" , "lead-guide") , tourController.deleteTour);
+    .patch(protectRoute, restrictTo("admin", "lead-guide"), tourController.updateTour)
+    .delete(protectRoute, restrictTo("admin", "lead-guide"), tourController.deleteTour);
 
 
 module.exports = router;
