0eadd4282add10f83a4577a1e13acc0a33d738ba
el3raky <kameladel339@gmail.com>
2024-02-22 00:43:28 +0200

    implement getOne , getAll factory for users

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,24 +5,31 @@ const displayValidationErrors = require("../utils/validationErrors");
 const sendResponse = require("../utils/sendResponse");
 const factory = require("./handlerFactory");
 
-exports.getAllUsers = asyncHandler(async (req, res, next) => {
 
-    const users = await User.find().select("-password")
+exports.getAllUsers = factory.getAll(User)
+// exports.getAllUsers = asyncHandler(async (req, res, next) => {
+
+//     const users = await User.find().select("-password")
 
 
-    // 3) send response to the client
-    sendResponse(res, 200, {
-        result: users.length,
-        data: { users }
-    })
-});
+//     // 3) send response to the client
+//     sendResponse(res, 200, {
+//         result: users.length,
+//         data: { users }
+//     })
+// });
 
-exports.getUser = (req, res) => {
-    // 3) send response to the client
-    sendResponse(res, 200, {
-        data: "get single user"
-    })
+exports.setUserId = (req, res, next) => {
+    req.params.id = req.user.id
+    next()
 }
+exports.getUser = factory.getOne(User)
+// exports.getUser = (req, res) => {
+//     // 3) send response to the client
+//     sendResponse(res, 200, {
+//         data: "get single user"
+//     })
+// }
 
 exports.createUser = asyncHandler(async (req, res) => {
 
@@ -33,9 +40,7 @@ exports.createUser = asyncHandler(async (req, res) => {
     // 3) send response to the client
     sendResponse(res, 201, {
         status: "success",
-        data: {
-            newUser
-        }
+        message: "this route is not yet defined, please use /signup instead",
     })
 });
 
@@ -55,19 +60,19 @@ exports.deleteUser = factory.deleteOne(User)
 // });
 
 exports.setUpdateData = (req, res, next) => {
-    
+
     const { email, name, role } = req.body
     req.updateData = { email, name, role }
     next()
 }
-exports.updateUser = factory.updateOne(User)
+exports.updateUser = factory.updateOne(User)  // don't update password with this handler
 // exports.updateUser = asyncHandler(async (req, res, next) => {
 
-    // // 1)  check if there is errors from express-validator
-    // const errors = validationResult(req)
-    // if (!errors.isEmpty()) {
-    //     return displayValidationErrors(errors, res)
-    // }
+// // 1)  check if there is errors from express-validator
+// const errors = validationResult(req)
+// if (!errors.isEmpty()) {
+//     return displayValidationErrors(errors, res)
+// }
 
 //     //2) update user data
 //     const { email, name, role } = req.body
