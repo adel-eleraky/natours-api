57c2a3cf9748a85df78ff1f9703224937b6ca059
el3raky <kameladel339@gmail.com>
2024-02-22 00:41:15 +0200

    added getOne , getAll factory

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -3,6 +3,54 @@ const sendResponse = require("./../utils/sendResponse")
 const asyncHandler = require("./../utils/asyncHandler")
 const displayValidationErrors = require("../utils/validationErrors");
 const { validationResult } = require("express-validator");
+const ApiFeatures = require("./../utils/apiFeatures")
+
+// factory get all
+exports.getAll = Model => asyncHandler(async (req, res, next) => {
+
+    //  to allow for nested GET reviews on specific tour ( /tours/:tourId/reviews)
+    let filter = {}
+    if (req.params.tourId) filter = { tour: req.params.tourId }
+
+    // 1) build the query
+    const features = new ApiFeatures(Model.find(filter), req.query)
+        .filter()
+        .sort()
+        .limitFields()
+        .paginate()
+
+    // 2) run the query
+    const docs = await features.query;
+    // const docs = await features.query.explain();
+
+    // 3) send response to the client
+    sendResponse(res, 200, {
+        result: docs.length,
+        data: { docs }
+    })
+
+})
+
+// factory get
+exports.getOne = (Model, popOptions) => asyncHandler(async (req, res, next) => {
+
+    let query = Model.findById(req.params.id);
+
+    if (popOptions) query = query.populate(popOptions);
+
+    const doc = await query;
+    // Tour.findOne({ _id: req.params.id })
+
+    if (!doc) {
+        return next(new AppError("No document found with this ID", 404, "fail"))
+    }
+
+    // send response to the client
+    sendResponse(res, 200, {
+        status: "success",
+        data: { doc }
+    })
+})
 
 
 // factory delete
@@ -17,7 +65,8 @@ exports.deleteOne = Model => asyncHandler(async (req, res, next) => {
     // send response to the client
     sendResponse(res, 204, {
         status: "success",
-        message: "document deleted successfully"
+        message: "document deleted successfully",
+        data: null
     })
 
 })
@@ -46,13 +95,14 @@ exports.updateOne = (Model) => asyncHandler(async (req, res, next) => {
     }
 
     const updateData = req.updateData ? req.updateData : req.body
-    const UpdatedDoc = await Model.findByIdAndUpdate(req.params.id, updateData , {
+    const UpdatedDoc = await Model.findByIdAndUpdate(req.params.id, updateData, {
         new: true,   // to return the updated doc
         runValidators: true,  // to run data validators again on update
     });
 
     // send response to the client
     sendResponse(res, 201, {
+        status: "success",
         message: "document updated successfully",
         data: {
             UpdatedDoc
