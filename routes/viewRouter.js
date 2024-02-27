22b409ef7641aae1f2421900d8c61a9c8771a3ea
el3raky <kameladel339@gmail.com>
2024-02-27 14:38:23 +0200

    added login route

--- routes/viewRouter.js
+++ routes/viewRouter.js
@@ -1,10 +1,12 @@
 const express = require("express")
 const viewsController = require("../controllers/viewsController")
+const { protectRoute } = require("../middlewares/authMiddleware")
 
 const router = express.Router()
 
 
 router.get("/" , viewsController.getOverview)
-router.get("/tour/:slug" , viewsController.getTour)
+router.get("/tour/:slug" , protectRoute ,viewsController.getTour)
+router.get("/login" , viewsController.getLoginForm)
 
 module.exports = router
