77383f4613a99431e84c2b408dc7be62d425549f
el3raky <kameladel339@gmail.com>
2024-02-29 10:44:56 +0200

    created isLoggedIn middleware

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -5,31 +5,31 @@ const jwt = require("jsonwebtoken")
 
 
 // middleware to prevent unauthorized access to the Route 
-exports.protectRoute = asyncHandler(async (req , res , next) => {
+exports.protectRoute = asyncHandler(async (req, res, next) => {
 
     let token;
 
     // 1) getting token from header
-    if(req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
+    if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
         token = req.headers.authorization.split(" ")[1];
-    } else if(req.cookies.jwt) {
+    } else if (req.cookies.jwt) {
         token = req.cookies.jwt
     }
-    
-    if(! token) return next(new AppError("You are not logged in , please log-in to get access" , 401 , "fail"))
-    
+
+    if (!token) return next(new AppError("You are not logged in , please log-in to get access", 401, "fail"))
+
     // 2) verify token
-    const decodedJWT = jwt.verify(token , process.env.JWT_SECRET)
+    const decodedJWT = jwt.verify(token, process.env.JWT_SECRET)
 
 
     // 3) check if the user belonging to this token still exist
     const user = await User.findById(decodedJWT.id).select("+password")
-    if(! user) return next(new AppError("the User belonging to this token doesn't exist" , 401 , "fail"))
+    if (!user) return next(new AppError("the User belonging to this token doesn't exist", 401, "fail"))
 
 
     // 4) check if user change password after token was issued
-    if(user.changedPasswordAfter(decodedJWT.iat)) {
-        return next(new AppError("user recently changed password! please log in again." , 401 , "fail"))
+    if (user.changedPasswordAfter(decodedJWT.iat)) {
+        return next(new AppError("user recently changed password! please log in again.", 401, "fail"))
     }
 
 
@@ -38,13 +38,39 @@ exports.protectRoute = asyncHandler(async (req , res , next) => {
     next()
 })
 
+// middleware to check if user is logged in
+exports.isLoggedIn = async (req, res, next) => {
+
+    try {
+
+        if (req.cookies.jwt) {
+
+            // 2) verify token
+            const decodedJWT = jwt.verify(req.cookies.jwt, process.env.JWT_SECRET)
+
+            // 3) check if the user belonging to this token still exist
+            const user = await User.findById(decodedJWT.id)
+            if (!user) return next()
+
+            // 4) check if user change password after token was issued
+            if (user.changedPasswordAfter(decodedJWT.iat)) return next()
+
+            // GRANT access to protected route
+            res.locals.user = user
+            return next()
+        }
+    } catch (err) {
+        return next()
+    }
+    next()
+}
 
 // middleware to restrict access permission to the Route
 exports.restrictTo = (...roles) => {
 
-    return (req , res , next) => {
-        if(! roles.includes(req.user.role)) {
-            return next(new AppError("You don't have permission to do this action" , 403 , "fail"))
+    return (req, res, next) => {
+        if (!roles.includes(req.user.role)) {
+            return next(new AppError("You don't have permission to do this action", 403, "fail"))
         }
 
         // GRANT ACCESS TO protected Route
