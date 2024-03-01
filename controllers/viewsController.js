31eeb9400f63812d7fe5a95f4edb9b3b533a42ad
el3raky <kameladel339@gmail.com>
2024-03-01 04:21:07 +0200

    created update user data handler

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,6 +1,7 @@
 const Tour = require("./../models/tourModel")
+const User = require("./../models/userModel")
 const asyncHandler = require("./../utils/asyncHandler")
-
+const AppError = require("./../utils/appError")
 
 exports.getOverview = asyncHandler(async (req, res, next) => {
 
@@ -23,15 +24,38 @@ exports.getTour = asyncHandler(async (req, res, next) => {
         fields: "review rating user",
     })
 
+    if (!tour) return next(new AppError("There is no tour with that name", 404, "fail"))
+
     res.status(200).render("tour", {
         title: `${tour.name} Tour`,
         tour
     })
 })
 
-exports.getLoginForm = asyncHandler(async (req , res , next) => {
+exports.getLoginForm = asyncHandler(async (req, res, next) => {
 
     res.status(200).render("login", {
         title: "Log into your account"
     })
+})
+
+exports.getAccount = asyncHandler(async (req, res, next) => {
+
+    res.status(200).render("account", {
+        title: "Your account"
+    })
+})
+
+exports.updateUserData = asyncHandler(async (req, res, next) => {
+
+    const updateData = {
+        name: req.body.name,
+        email: req.body.email
+    }
+    const newUser = await User.findByIdAndUpdate(req.user.id, updateData, {
+        new: true,
+        runValidators: true
+    })
+
+    res.status(200).redirect("/me")
 })
