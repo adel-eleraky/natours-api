43703d847aff5f5909d9b783ac2c5c16f5a27bc6
el3raky <kameladel339@gmail.com>
2024-03-01 04:28:42 +0200

    created route to profile page

--- routes/viewRouter.js
+++ routes/viewRouter.js
@@ -1,13 +1,15 @@
 const express = require("express")
 const viewsController = require("../controllers/viewsController")
-const { protectRoute , isLoggedIn } = require("../middlewares/authMiddleware")
+const { protectRoute, isLoggedIn } = require("../middlewares/authMiddleware")
 
 const router = express.Router()
 
-router.use(isLoggedIn)
+// router.use(isLoggedIn)
 
-router.get("/" , viewsController.getOverview)
-router.get("/tour/:slug" ,viewsController.getTour)
-router.get("/login" , viewsController.getLoginForm)
+router.get("/", isLoggedIn, viewsController.getOverview)
+router.get("/tour/:slug", isLoggedIn, viewsController.getTour)
+router.get("/login", isLoggedIn, viewsController.getLoginForm)
+router.get("/me", protectRoute, viewsController.getAccount)
+router.post("/submit-user-data" , protectRoute, viewsController.updateUserData)
 
 module.exports = router
