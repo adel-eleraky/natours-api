a5e9216abffea713ddd5743b2b98535023c09471
el3raky <kameladel339@gmail.com>
2024-03-13 07:12:57 +0200

    added get my tours route

--- routes/viewRouter.js
+++ routes/viewRouter.js
@@ -1,15 +1,17 @@
 const express = require("express")
 const viewsController = require("../controllers/viewsController")
 const { protectRoute, isLoggedIn } = require("../middlewares/authMiddleware")
+const bookingController = require("../controllers/bookingController")
 
 const router = express.Router()
 
 // router.use(isLoggedIn)
 
-router.get("/", isLoggedIn, viewsController.getOverview)
+router.get("/", bookingController.createBookingCheckout, isLoggedIn, viewsController.getOverview)
 router.get("/tour/:slug", isLoggedIn, viewsController.getTour)
 router.get("/login", isLoggedIn, viewsController.getLoginForm)
 router.get("/me", protectRoute, viewsController.getAccount)
-router.post("/submit-user-data" , protectRoute, viewsController.updateUserData)
+router.get("/my-tours", protectRoute, viewsController.getMyTours)
+router.post("/submit-user-data", protectRoute, viewsController.updateUserData)
 
 module.exports = router
